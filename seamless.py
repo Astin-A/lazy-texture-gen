7629ccc03893b4602955bfacd490553b61a6ff84
Ali Astin Azarafza <astinazarafza@gmail.com>
2023-12-26 17:07:05 -0500

    ignore generated files and dont regenerate

--- seamless.py
+++ seamless.py
@@ -1,7 +1,19 @@
 import sys
+import os
 from PIL import Image
 
 for img_fn in sys.argv[1:]:
+    # ignore generated files
+    if img_fn.find("_seamless.") != -1:
+        continue
+    
+    simg_fn = img_fn.replace("[.]","_seamless.")
+    
+    # seamless version already exists, dont regenerate
+    if os.path.isfile(simg_fn):
+        print("Seamless image already exists, ignoring %s..." % img_fn)
+        continue
+    
     img = Image.open(img_fn)
     print("Converting %s..." % img_fn)
     sz = img.size
@@ -9,16 +21,16 @@ for img_fn in sys.argv[1:]:
     for i in range(4):
         region += [img.crop((0,0,sz[0],sz[1]))]
     img = img.resize((sz[0] * 2, sz[1] * 2))
-
+    
     region[1] = region[1].transpose(Image.FLIP_TOP_BOTTOM)
-
+    
     region[2] = region[2].transpose(Image.FLIP_LEFT_RIGHT)
-
+    
     region[3] = region[3].transpose(Image.FLIP_TOP_BOTTOM)
     region[3] = region[3].transpose(Image.FLIP_LEFT_RIGHT)
-
+    
     img.paste(region[0], ((0,0,sz[0],sz[1])))
     img.paste(region[1], ((0,sz[1],sz[0],sz[1]*2)))
     img.paste(region[2], ((sz[0],0,sz[0]*2,sz[1])))
     img.paste(region[3], ((sz[0],sz[1],sz[0]*2,sz[1]*2)))
-    img.save(img_fn.replace(".","_seamless."))
+    img.save(simg_fn)
